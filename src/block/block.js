0e2128699f8251d1e22074288870b86a262fb109
Vineeta Sharma <vineetasharma18791@gmail.com>
2018-06-12 13:06:43 +0530

    Buttons alignment updated

--- src/block/block.js
+++ src/block/block.js
@@ -28,21 +28,36 @@ class Block extends Component {
                 style={{
                     margin: 10,
                     backgroundColor: "#a1a1a1",
-                    width: this.props.width
+                    width: this.props.width,
+                    height: 100
                 }}
                 onMouseEnter={this.onMouseEnterHandler}
                 onMouseLeave={this.onMouseLeaveHandler}
             >
+                <div>
+                    <p style={{"padding":5}}>
+                        {this.props.item.title}
+                    </p>
+                    <p style={{"padding":5}}>
+                        {this.props.item.desc}
+                    </p>
+                </div>
+
                 {
-                    this.state.hover ? (<div>
-                        <span>Open</span>
-                        <span>Print</span>
-                        <span onClick={this.delete}>Delete</span>
+                    this.state.hover ? (<div
+                        style={{
+                            top: -100,
+                            zIndex: 1000,
+                            position: "relative",
+                            left: 5,
+                            display: "flex"
+                        }}
+                    >
+                        <span style={{padding: 5, cursor: "not-allowed", margin: 5, borderRadius: 4, backgroundColor: "#b1b1b1", color:"#fff"}}>Open</span>
+                        <span style={{padding: 5,    cursor: "not-allowed", margin: 5, borderRadius: 4, backgroundColor: "#b1b1b1", color:"#fff"}}>Print</span>
+                        <span style={{padding: 5,    cursor: "pointer", margin: 5, borderRadius: 4, backgroundColor: "#b1b1b1", color:"#fff"}} onClick={this.delete}>X</span>
                     </div>): <div/>
                 }
-                <p style={{"padding":5}}>
-                    {this.props.item.title}
-                </p>
             </div>
         );
     }
