db313adfb10f4709d55be27e5eeaff753e9be6cd
Vineeta Sharma <vineetasharma18791@gmail.com>
2018-06-12 14:25:36 +0530

    Icon & background image added to blocks

--- src/block/block.js
+++ src/block/block.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import icon from '../images/logo.png';
+import backgroundImg from '../images/background.jpeg';
 
 class Block extends Component {
     constructor(){
@@ -29,24 +31,35 @@ class Block extends Component {
                     margin: 10,
                     backgroundColor: "#a1a1a1",
                     width: this.props.width,
-                    height: 100
+                    height: 100,
+                    backgroundImage: `url(${backgroundImg})`
                 }}
                 onMouseEnter={this.onMouseEnterHandler}
                 onMouseLeave={this.onMouseLeaveHandler}
             >
                 <div>
-                    <p style={{"padding":5}}>
-                        {this.props.item.title}
-                    </p>
-                    <p style={{"padding":5}}>
-                        {this.props.item.desc}
-                    </p>
+                    <div
+                        style={{
+                            display: "inline-flex"
+                        }}
+                    >
+                        <img src={icon} width="50" height="50" style={{"padding":5}} />
+                        <h3>
+                            {this.props.item.title}
+                        </h3>
+                    </div>
+                    <div>
+                        <p style={{"padding":5}}>
+                            {this.props.item.desc}
+                        </p>
+                    </div>
+
                 </div>
 
                 {
                     this.state.hover ? (<div
                         style={{
-                            top: -100,
+                            top: -120,
                             zIndex: 1000,
                             position: "relative",
                             left: 5,
@@ -55,7 +68,7 @@ class Block extends Component {
                     >
                         <span style={{padding: 5, cursor: "not-allowed", margin: 5, borderRadius: 4, backgroundColor: "#b1b1b1", color:"#fff"}}>Open</span>
                         <span style={{padding: 5,    cursor: "not-allowed", margin: 5, borderRadius: 4, backgroundColor: "#b1b1b1", color:"#fff"}}>Print</span>
-                        <span style={{padding: 5,    cursor: "pointer", margin: 5, borderRadius: 4, backgroundColor: "#b1b1b1", color:"#fff"}} onClick={this.delete}>X</span>
+                        <span style={{padding: 5,    cursor: "pointer", margin: 5, borderRadius: 4, backgroundColor: "#b1b1b1", color:"#f00"}} onClick={this.delete}>X</span>
                     </div>): <div/>
                 }
             </div>
